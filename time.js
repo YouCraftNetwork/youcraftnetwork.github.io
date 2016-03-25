f2b247d29312e40d9aad43d3d4725f129e5cbba2
youcraftnetwork <youcraftnetwork@users.noreply.github.com>
2016-03-24 19:19:30 -0800

    Added files via upload

---
+++ time.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+TargetDate = "04/01/2016 10:00 AM";
+BackColor = "#f1f1f1";
+ForeColor = "#f1f1f1";
+CountActive = true;
+CountStepper = -1;
+LeadingZero = true;
+DisplayFormat = "%%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds.";
+FinishMessage = "";
+//]]>
