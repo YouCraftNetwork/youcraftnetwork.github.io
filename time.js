d3f2de5082c3cfbdfcfd73c30dd35de3aa2bf7c0
youcraftnetwork <youcraftnetwork@users.noreply.github.com>
2016-03-24 19:30:55 -0800

    Update time.js

--- time.js
+++ time.js
@@ -1,7 +1,7 @@
 //<![CDATA[
 TargetDate = "04/01/2016 10:00 AM";
-BackColor = "#f1f1f1";
-ForeColor = "#f1f1f1";
+BackColor = "#ffffff";
+ForeColor = "#ffffff";
 CountActive = true;
 CountStepper = -1;
 LeadingZero = true;
