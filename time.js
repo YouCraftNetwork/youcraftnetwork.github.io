8e317eab30f626cfb070e84b40d6c353cec6816d
frts <youcraftnetwork@users.noreply.github.com>
2016-03-26 14:15:02 -0800

    Update time.js

--- time.js
+++ time.js
@@ -1,7 +1,7 @@
 //<![CDATA[
 TargetDate = "04/01/2016 10:00 AM";
-BackColor = "#ffffff";
-ForeColor = "#ffffff";
+BackColor = "#f1f1f1";
+ForeColor = "#f1f1f1";
 CountActive = true;
 CountStepper = -1;
 LeadingZero = true;
